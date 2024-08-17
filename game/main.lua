e2f3328c771ed330e736c7078b66c56f5a608ea8
giuseppe.lantieri <giuseppelantieri34@gmail.com>
2024-08-17 18:08:45 +0200

    init world with windfield

--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,25 @@
+wf = require 'lib/windfield'
+
 function love.load()
+	world = wf.newWorld(0, 0, true)
+	world:setGravity(0, 512)
+
+	box = world:newRectangleCollider(400 - 50 / 2, 0, 50, 50)
+	box:setRestitution(0.8)
+	box:applyAngularImpulse(5000)
+
+	ground = world:newRectangleCollider(0, 550, 800, 50)
+	wall_left = world:newRectangleCollider(0, 0, 50, 600)
+	wall_right = world:newRectangleCollider(750, 0, 50, 600)
+	ground:setType('static') -- Types can be 'static', 'dynamic' or 'kinematic'. Defaults to 'dynamic'
+	wall_left:setType('static')
+	wall_right:setType('static')
 end
 
-function love.update()
-	print("Hello!")
+function love.update(dt)
+	world:update(dt)
 end
 
 function love.draw()
+	world:draw()
 end
